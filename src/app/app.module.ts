2bd9c56ad0c27b5e688fde1afeae78d52328cd40
Shaijil-geon <shaijil.pb@gadgeon.com>
2018-01-24 15:50:07 +0530

    navigation module added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from  './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      NavigationComponent      
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html'   
+})
+export class NavigationComponent {
+
+}
